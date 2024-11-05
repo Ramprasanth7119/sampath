eaf270167570914f6dfa2520b2fa6769c3a73437
Ramprasanth7119 <151420291+Ramprasanth7119@users.noreply.github.com>
2024-11-05 11:03:27 +0530

    Update Manufacture.jsx

--- src/components/Manufacture.jsx
+++ src/components/Manufacture.jsx
@@ -17,49 +17,49 @@ function Manufacture() {
       </header>
 
       <section className="card-grid">
-        <div className="card large" style={{ backgroundImage: 'url(../src/assets/images/img1.jpeg)' }}>
+        <div className="card large" style={{ backgroundImage: 'url(../assets/images/img1.jpeg)' }}>
           <div className="card-content">
             <h3>Work with focus (or friends)</h3>
             <p>Design in private and by yourself. Or co-edit in real-time. The choice is always yours.</p>
           </div>
         </div>
-        <div className="card large" style={{ backgroundImage: ' url(../src/assets/images/img2.jpeg)' }}>
+        <div className="card large" style={{ backgroundImage: ' url(../assets/images/img2.jpeg)' }}>
           <div className="card-content">
             <h3>Every point perfect</h3>
             <p>Create anything from icons to entire apps with a vector editor like no other.</p>
           </div>
         </div>
-        <div className="card small" style={{ backgroundImage: 'url(../src/assets/images/img3.avif)' }}>
+        <div className="card small" style={{ backgroundImage: 'url(../assets/images/img3.avif)' }}>
           <div className="card-content">
             <h3>Work effortlessly</h3>
             <p>Easy multi-layer renaming, a Canvas Minimap, and Command Bar put your workflow on rails.</p>
           </div>
         </div>
-        <div className="card small" style={{ backgroundImage: 'url(../src/assets/images/img5.jpeg)' }}>
+        <div className="card small" style={{ backgroundImage: 'url(../assets/images/img5.jpeg)' }}>
           <div className="card-content">
             <h3>Offline? Any time</h3>
             <p>Disconnect and design without compromises. Work locally, without internet, on your terms.</p>
           </div>
         </div>
-        <div className="card small" style={{ backgroundImage: 'url(../src/assets/images/img3.avif)' }}>
+        <div className="card small" style={{ backgroundImage: 'url(../assets/images/img3.avif)' }}>
           <div className="card-content">
             <h3>Offline? Any time</h3>
             <p>Disconnect and design without compromises. Work locally, without internet, on your terms.</p>
           </div>
         </div>
-        <div className="card small" style={{ backgroundImage: 'url(../src/assets/images/img5.jpeg)' }}>
+        <div className="card small" style={{ backgroundImage: 'url(../assets/images/img5.jpeg)' }}>
           <div className="card-content">
             <h3>Work effortlessly</h3>
             <p>Easy multi-layer renaming, a Canvas Minimap, and Command Bar put your workflow on rails.</p>
           </div>
         </div>
-        <div className="card large" style={{ backgroundImage: 'url(../src/assets/images/img7.jpeg)' }}>
+        <div className="card large" style={{ backgroundImage: 'url(../assets/images/img7.jpeg)' }}>
           <div className="card-content">
             <h3>Ready to scale</h3>
             <p>Reuse elements across your designs. Update once and watch your changes roll out everywhere.</p>
           </div>
         </div>
-        <div className="card large" style={{ backgroundImage: 'url(../src/assets/images/img8.webp)' }}>
+        <div className="card large" style={{ backgroundImage: 'url(../assets/images/img8.webp)' }}>
           <div className="card-content">
             <h3>Zero to done</h3>
             <p>Get started fast with 100s of free templates, Artboard presets, and component libraries.</p>
